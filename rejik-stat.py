c1633ff54db17d7c77396b1bc0624572174647a4
Oleg Palij <o.palij@gmail.com>
2005-12-04 19:21:06 +0000

    added working version

--- rejik-stat.py
+++ rejik-stat.py
@@ -1,32 +1,42 @@
 #!/usr/local/bin/python
 
 def parse_line(line):
+    line = line.replace('\n','')
     line = line.split()
-    date_time = line[0] + ' ' + line[1]
     category = line[2].replace(':','')
     user_ip = line[3]
     user_name = line[4]
-    url = line[5]
-    return { 'logTime': date_time, 'category': category, 'user_ip': user_ip, 'user_name': user_name, 'url': url }
+    return { 'category': category, 'user_ip': user_ip, 'user_name': user_name }
 
 fh = open('redirector.log')
 
-line = fh.readline()
-users = []
-user_ips = []
-categoryes = []
+db = []
 
-while line != '':
-      stats = parse_line(line)
-      if stats['user_ip'] not in user_ips:
-         user_ips.append(stats['user_ip'])
-      if stats['user_name'] not in users:
-         users.append(stats['user_name']
-      if stats['category'] not in categoryes:
-         categoryes.append(stats['category']
-      line = line.replace('\n','')
+line = fh.readline()
+stats = parse_line(line)
+db.append( { 'user_ip': stats['user_ip'], 'user_name':stats['user_name'], 'category': stats['category'], 'hits': 1 } )
+
+while True:
       line = fh.readline()
+      if line == '':
+         break
+      else:
+         stats = parse_line(line)
+
+      HIT = False
+
+      lenght = len(db)
+      for i in xrange(0, lenght):
+          if (stats['user_ip'] == db[i]['user_ip']) and (stats['user_name'] == db[i]['user_name']) and (stats['category'] == db[i]['category']):
+             HIT = True
+             break
+
+      if HIT:
+         db[i]['hits'] += 1
+      else:
+         db.append( { 'user_ip': stats['user_ip'], 'user_name':stats['user_name'], 'category': stats['category'], 'hits': 1 } )
 
 fh.close()
 
-print categoryes
+for i in xrange(0, lenght):
+    print db[i]
