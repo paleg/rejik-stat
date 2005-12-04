50c43444460928ea83832e3d8e7b81c29218b9a4
Oleg Palij <o.palij@gmail.com>
2005-12-04 20:51:35 +0000

    added formated output

--- rejik-stat.py
+++ rejik-stat.py
@@ -1,4 +1,6 @@
 #!/usr/local/bin/python
+HITS_ROWS = 5
+NAMES_ROWS = 30
 
 def parse_line(line):
     line = line.replace('\n','')
@@ -8,9 +10,9 @@ def parse_line(line):
     user_name = line[4]
     return { 'category': category, 'user_ip': user_ip, 'user_name': user_name }
 
-def get_user_category_hits(db, user_ip, category):
+def get_user_category_hits(db, user_ip, user_name, category):
     for i in xrange(0, len(db)):
-        if user_ip == db[i]['user_ip'] and category == db[i]['category']:
+        if (user_ip == db[i]['user_ip']) and (user_name == db[i]['user_name']) and(category == db[i]['category']):
            return db[i]['hits']
     return 0 
 
@@ -18,12 +20,13 @@ fh = open('redirector.log')
 
 db = []
 categoryes = []
-user_ips = []
+users = []
 
 line = fh.readline()
 stats = parse_line(line)
 db.append( { 'user_ip': stats['user_ip'], 'user_name':stats['user_name'], 'category': stats['category'], 'hits': 1 } )
-categoryes.append(stats['category'])
+categoryes.append({ 'category': stats['category'], 'len': len(stats['category']) } )
+users.append( { 'user_ip': stats['user_ip'], 'user_name': stats['user_name'] } )
 
 while True:
       line = fh.readline()
@@ -32,10 +35,27 @@ while True:
       else:
          stats = parse_line(line)
 
-      if stats['category'] not in categoryes:
-         categoryes.append(stats['category'])
-      if stats['user_ip'] not in user_ips:
-         user_ips.append(stats['user_ip'])
+      HIT = False
+      for i in xrange(0, len(categoryes)):
+         if stats['category'] == categoryes[i]['category']:
+            HIT = True
+            break
+
+      if not HIT:
+         if len(stats['category']) < HITS_ROWS:
+            lenght = HITS_ROWS 
+         else:
+            lenght = len(stats['category']) 
+         categoryes.append( { 'category': stats['category'], 'len': lenght } )
+
+      HIT = False
+      for i in xrange(0, len(users)):
+          if stats['user_ip'] == users[i]['user_ip'] and stats['user_name'] == users[i]['user_name']:
+             HIT = True
+             break
+
+      if not HIT:
+         users.append( { 'user_ip': stats['user_ip'], 'user_name': stats['user_name'] } )
 
       HIT = False
 
@@ -51,9 +71,21 @@ while True:
 
 fh.close()
 
-print categoryes
-for user_ip in user_ips:
-    print user_ip,
-    for category in categoryes:
-        print get_user_category_hits(db, user_ip, category),
-    print
+field = 'IP (UserName)'
+print field.ljust(NAMES_ROWS),
+
+for i in xrange(len(categoryes)):
+    print categoryes[i]['category'].center(int(categoryes[i]['len'])),
+print
+
+for i in xrange(0, len(users)):
+    if users[i]['user_name'] == '-':
+       field = '%s'%(users[i]['user_ip'])
+    else:
+       field = str('%s (%s)'%(users[i]['user_ip'], users[i]['user_name']))
+    print field.ljust(NAMES_ROWS),
+
+    for j in xrange(len(categoryes)):
+        hits = str(get_user_category_hits(db, users[i]['user_ip'], users[i]['user_name'], categoryes[j]['category']))
+        print hits.center(categoryes[j]['len']),
+    print 
