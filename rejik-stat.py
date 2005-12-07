752813227224fc8b5c8aca975c915e4e42fdd2d6
Oleg Palij <o.palij@gmail.com>
2005-12-07 12:11:52 +0000

    Mega commit for rejik-log

--- rejik-stat.py
+++ rejik-stat.py
@@ -1,18 +1,18 @@
 #!/usr/local/bin/python
-# Minimun numbers of rows in categoryes
+# Minimun numbers of rows in categories
 HITS_ROWS = 5
 # Numbers of rows in user_ip (user_name)
 NAMES_ROWS = 28 
 
-USAGE = """
-$ rejik-stat.py -f log_file [-d]
-rejik-stat (Get info in human-readable format from rejik log file)
-by Oleg Palij <xmpp://malik@jabber.te.ua>"""
+USAGE = """$ rejik-stat.py [-h] -f log_file [-d] [--strip-domain] [-c <categories>] [-u <users>]
+rejik-stat (Get info in human-readable format from rejik log) by Oleg Palij <xmpp://malik@jabber.te.ua>
+Example: ./rejik-stat.py --strip-domain -c PORNO,CHAT,IP -u o.palij,10.6.44.38 -f redirector.log
+"""
 
 # Main data store - dictionary  { 'user_ip':, 'user_name':, 'category':, 'hits': }. 'hits' - number of hits to 'category' by 'user_name' from 'user_ip'
 db = []
-# data store for categoryes - dictionary  { 'category': , 'len': }. 'len' - number of rows ranked by 'category' 
-categoryes = []
+# data store for categories - dictionary  { 'category': , 'len': }. 'len' - number of rows ranked by 'category' 
+categories = []
 # data store for users - dictionary { 'user_ip': , 'user_name': }
 users = []
 
@@ -44,22 +44,40 @@ from optparse import OptionParser
 from sys import stderr, exit
 
 parser = OptionParser(usage=USAGE)
-parser.add_option("-f", dest="log_file", default='redirector.log',
+parser.add_option("-f", dest="log_file",
                      help="rejik log file name")
 parser.add_option("-d", "--debug",
                      action="store_true", dest="DEBUG", default=False,
                      help="go into debug mode")
+parser.add_option("-u", dest="USERS", type="string",
+                     help="To show only that categories that comma-separated list includes")
+parser.add_option("-c", dest="CATEGORIES", type="string",
+                     help="To show only that users that comma-separated list includes")
 parser.add_option("--strip-domains",
                      action="store_true", dest="STRIP_DOMAINS", default=False,
                      help="strip domain part in user name")
 
 (options, args) = parser.parse_args()
 
+if options.log_file == None:
+   print USAGE
+   exit(1)
+
 try:
    fh = open(options.log_file)
 except:
    print >> stderr, 'Problems while opening rejik log file %s'%(options.log_file)
    exit(1)
+
+try:
+   options.CATEGORIES = options.CATEGORIES.strip(',')
+except:
+   options.CATEGORIES = [] 
+
+try:
+   options.USERS = options.USERS.strip(',')
+except:
+   options.USERS = []
 
 if options.STRIP_DOMAINS:
    import re
@@ -71,45 +89,48 @@ while True:
       else:
          stats = parse_line(line)
 
-      """ Gather all unique categoryes into list of dictionaryes 
-          categoryes[ { 'category':, 'len': }, ... ]
+      """ Gather all unique categories into list of dictionaryes 
+          categories[ { 'category':, 'len': }, ... ]
       """
-      HIT = False
-      for i in xrange(0, len(categoryes)):
-         if stats['category'] == categoryes[i]['category']:
-            HIT = True
-            break
-      if not HIT:
-         if len(stats['category']) < HITS_ROWS:
-            lenght = HITS_ROWS 
-         else:
-            lenght = len(stats['category']) 
-         categoryes.append( { 'category': stats['category'], 'len': lenght } )
+      if (stats['category'] in options.CATEGORIES) or (options.CATEGORIES == []):
+         HIT = False
+         for i in xrange(0, len(categories)):
+            if stats['category'] == categories[i]['category']:
+               HIT = True
+               break
+         if not HIT:
+            if len(stats['category']) < HITS_ROWS:
+               lenght = HITS_ROWS 
+            else:
+               lenght = len(stats['category']) 
+            categories.append( { 'category': stats['category'], 'len': lenght } )
 
       """ Gather all unique 'user_ip' 'user_name' pairs into list of dictionaryes 
           users[ { 'user_ip':, 'user_name': }, ... ]
       """
-      HIT = False
-      for i in xrange(0, len(users)):
-          if stats['user_ip'] == users[i]['user_ip'] and stats['user_name'] == users[i]['user_name']:
-             HIT = True
-             break
-      if not HIT:
-         users.append( { 'user_ip': stats['user_ip'], 'user_name': stats['user_name'] } )
+      if (stats['user_ip'] in options.USERS) or (stats['user_name'] in options.USERS) or (options.USERS == []):
+         HIT = False
+         for i in xrange(0, len(users)):
+             if stats['user_ip'] == users[i]['user_ip'] and stats['user_name'] == users[i]['user_name']:
+                HIT = True
+                break
+         if not HIT:
+            users.append( { 'user_ip': stats['user_ip'], 'user_name': stats['user_name'] } )
 
       """ Gather hits for all unique pairs 'user_ip' 'user_name' in 'category' into list of dictionaryes
           db[ { 'user_ip':, 'user_name', 'category', 'hits' }, ... ]
       """
-      HIT = False
-      for i in xrange(0, len(db)):
-          if (stats['user_ip'] == db[i]['user_ip']) and (stats['user_name'] == db[i]['user_name']) and (stats['category'] == db[i]['category']):
-             HIT = True
-             break
-      # if 'user_ip'&'user_name' in 'category' is already in db - increment hits, else - add new entry
-      if HIT:
-         db[i]['hits'] += 1
-      else:
-         db.append( { 'user_ip': stats['user_ip'], 'user_name':stats['user_name'], 'category': stats['category'], 'hits': 1 } )
+      if ((stats['user_ip'] in options.USERS) or (stats['user_name'] in options.USERS) or (options.USERS == [])) and ((stats['category'] in options.CATEGORIES) or (options.CATEGORIES == [])):
+         HIT = False
+         for i in xrange(0, len(db)):
+             if (stats['user_ip'] == db[i]['user_ip']) and (stats['user_name'] == db[i]['user_name']) and (stats['category'] == db[i]['category']):
+                HIT = True
+                break
+         # if 'user_ip'&'user_name' in 'category' is already in db - increment hits, else - add new entry
+         if HIT:
+            db[i]['hits'] += 1
+         else:
+            db.append( { 'user_ip': stats['user_ip'], 'user_name':stats['user_name'], 'category': stats['category'], 'hits': 1 } )
 
 fh.close()
 
@@ -117,9 +138,9 @@ fh.close()
 field = 'IP (UserName)'
 print field.ljust(NAMES_ROWS),
 
-# Print title (categoryes)
-for i in xrange(len(categoryes)):
-    print categoryes[i]['category'].center(int(categoryes[i]['len'])),
+# Print title (categories)
+for i in xrange(len(categories)):
+    print categories[i]['category'].center(int(categories[i]['len'])),
 print
 
 for i in xrange(0, len(users)):
@@ -131,7 +152,10 @@ for i in xrange(0, len(users)):
     print field.ljust(NAMES_ROWS),
 
     # Print hists to 'category' for 'user_ip'&'user_name'
-    for j in xrange(len(categoryes)):
-        hits = str(get_user_category_hits(db, users[i]['user_ip'], users[i]['user_name'], categoryes[j]['category']))
-        print hits.center(categoryes[j]['len']),
+    for j in xrange(len(categories)):
+        hits = str(get_user_category_hits(db, users[i]['user_ip'], users[i]['user_name'], categories[j]['category']))
+        print hits.center(categories[j]['len']),
     print 
+
+print
+exit(0)
